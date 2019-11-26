1ccd499ffadf8cd15a0ac15d7d6c1b4f7bd44b18
Eduardo Valerio Gamez <evaleriog@outlook.com>
2019-11-26 11:32:37 -0600

    OOP Exercises part 2

--- src/Person.java
+++ src/Person.java
@@ -18,10 +18,25 @@ public class Person {
 
     // prints a message to the console using the person's name
     public void sayHello(){
-        System.out.printf("Hello friend...my name is %s%n", this.name;);
+        System.out.printf("Hello friend...my name is %s%n", this.name);
     }
 
     public static void main(String[] args){
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
 
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
     }
 }
