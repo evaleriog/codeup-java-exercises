b13aa732ae971f76795152994c19034cdece9e2c
Eduardo Valerio Gamez <evaleriog@outlook.com>
2019-12-06 15:48:45 -0600

    Java Exceptions and Error Handling Exercises

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -4,11 +4,13 @@ public class InputTest {
     public static void main(String[] args){
         Input input = new Input();
 
-        System.out.println(input.getString());
-        System.out.println(input.yesNo());
-        System.out.println(input.getInt());
+//        System.out.println(input.getString());
+//        System.out.println(input.yesNo());
+//        System.out.println(input.getInt());
         System.out.println(input.getInt(30,50));
-        System.out.println(input.getDouble());
-        System.out.println(input.getDouble(3000, 45000));
+//        System.out.println(input.getDouble());
+        System.out.println(input.getDouble(10, 40));
+        System.out.println(input.getBinary());
+        System.out.println(input.getHex());
     }
 }
